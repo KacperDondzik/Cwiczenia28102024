8149418114c59f38ac9f28567f64307c7147cb61
Kacper Dondzik <kdondzik@s201.local>
2024-10-28 08:29:03 +0100

    skonczone zad 2.2

--- Cwiczenia28102024/MainWindow.xaml.cs
+++ Cwiczenia28102024/MainWindow.xaml.cs
@@ -27,7 +27,9 @@ namespace Cwiczenia28102024
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("witaj, świecie!");
+            this.Opacity = 0.5; 
+            MessageBox.Show("Witaj, świecie!"); 
+            this.Opacity = 1.0;
         }
 
         private void btnTime_MouseEnter(object sender, MouseEventArgs e)
